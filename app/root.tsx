3e1ee7fc0f255fe2d04abb9e709e04bb9503cc31
Igor Makarov <igormaka@gmail.com>
2026-03-21 23:11:45 +0200

    safe area?

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ export default function Root() {
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         <Meta />
         <Links />
       </head>
