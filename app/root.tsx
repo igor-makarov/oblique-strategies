e1e4344b9779c40f4853d27a5e160c679ea3ef6f
Igor Makarov <igormaka@gmail.com>
2026-03-22 15:29:54 +0200

    wip

--- app/root.tsx
+++ app/root.tsx
@@ -8,10 +8,10 @@ export default function Root() {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="theme-color" content="#f6f3ea" />
-        <link rel="manifest" href="manifest.webmanifest" />
+        <link rel="manifest" href="/manifest.webmanifest" />
         <Meta />
         <Links />
       </head>
