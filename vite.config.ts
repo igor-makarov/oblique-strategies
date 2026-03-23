4293f2a683f8dae54f284c50b36062ce048b2284
Igor Makarov <igormaka@gmail.com>
2026-03-23 13:19:34 +0200

    move dev hosts to plugin, env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,31 @@
 import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, type Plugin } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+function lanServer(): Plugin {
+  return {
+    name: "lan-server",
+    config({ server }, { mode }) {
+      // Reads VITE_ALLOWED_HOSTS from .env.local
+      const env = loadEnv(mode, process.cwd());
+      if (mode !== "development") return;
+      if (server?.allowedHosts != null) return;
+      const allowedHosts = env.VITE_ALLOWED_HOSTS?.split(",");
+      if (!allowedHosts?.length) return;
+      return { server: { host: true, allowedHosts } };
+    },
+  };
+}
 
 export default defineConfig({
   build: {
     assetsDir: (process.env.BASE_URL || "/").substring(1) + "assets/",
   },
-  server: {
-    allowedHosts: ["igors-macbook-air.local", "il-m-igor-ma-02.local"],
-    host: true,
-  },
-  plugins: [devtoolsJson(), tsconfigPaths(), reactRouter()],
+  plugins: [
+    lanServer(),
+    devtoolsJson(),
+    tsconfigPaths(),
+    reactRouter(),
+  ],
 });
