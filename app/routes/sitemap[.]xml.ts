89561d2ee4ca1bdb6ba071728bb5f769673c41ac
Igor Makarov <igormaka@gmail.com>
2026-03-23 15:18:18 +0200

    fix sitemap

--- app/routes/sitemap[.]xml.ts
+++ app/routes/sitemap[.]xml.ts
@@ -2,9 +2,9 @@ import { generateRemixSitemap } from "@forge42/seo-tools/remix/sitemap";
 import { routes } from "virtual:react-router/server-build";
 
 export const loader = async () => {
-  const domain = process.env.SITE_URL;
+  const domain = process.env.SITEMAP_BASE_URL;
   if (!domain) {
-    throw new Error("SITE_URL environment variable is required");
+    throw new Error("SITEMAP_BASE_URL environment variable is required");
   }
 
   const sitemap = await generateRemixSitemap({ domain, routes });
