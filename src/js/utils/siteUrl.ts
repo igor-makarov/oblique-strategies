662fa40de57aa4b17192b28790850ae93f5191f6
Igor Makarov <igormaka@gmail.com>
2026-03-28 18:47:14 +0300

    Add Cloudflare Pages worker proof of concept

--- src/js/utils/siteUrl.ts
+++ src/js/utils/siteUrl.ts
@@ -1,8 +1,8 @@
 export function getSiteOrigin(): string {
-  const configuredOrigin = process.env.SITEMAP_BASE_URL;
+  const configuredOrigin = process.env.CF_PAGES_URL || process.env.SITEMAP_BASE_URL;
 
   if (!configuredOrigin) {
-    throw new Error("SITEMAP_BASE_URL environment variable is required");
+    throw new Error("CF_PAGES_URL or SITEMAP_BASE_URL environment variable is required");
   }
 
   return configuredOrigin.replace(/\/$/, "");
