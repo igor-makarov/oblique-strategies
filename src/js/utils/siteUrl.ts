0a98a0228db9a72d267c0a886b3f0856779db652
Igor Makarov <igormaka@gmail.com>
2026-03-28 21:16:14 +0300

    add workers branch

--- src/js/utils/siteUrl.ts
+++ src/js/utils/siteUrl.ts
@@ -1,8 +1,13 @@
 export function getSiteOrigin(): string {
-  const configuredOrigin = process.env.CF_PAGES_URL || process.env.SITEMAP_BASE_URL;
+  let configuredOrigin: string;
+  if (process.env.WORKERS_CI_BRANCH && process.env.WORKERS_CI_BRANCH != "main") {
+    configuredOrigin = `https://${process.env.WORKERS_CI_BRANCH}-oblique-strategies-w.igormaka.workers.dev/`;
+  } else {
+    configuredOrigin = process.env.CF_PAGES_URL || process.env.SITEMAP_BASE_URL;
+  }
 
   if (!configuredOrigin) {
-    throw new Error("CF_PAGES_URL or SITEMAP_BASE_URL environment variable is required");
+    throw new Error("WORKERS_CI_BRANCH or CF_PAGES_URL or SITEMAP_BASE_URL environment variable is required");
   }
 
   return configuredOrigin.replace(/\/$/, "");
