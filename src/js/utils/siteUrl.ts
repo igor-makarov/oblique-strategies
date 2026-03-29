590a2289b3f8c443ed85bcd80b1c11430df23eda
Igor Makarov <igormaka@gmail.com>
2026-03-29 09:45:41 +0300

    cleanup

--- src/js/utils/siteUrl.ts
+++ src/js/utils/siteUrl.ts
@@ -1,13 +1,8 @@
 export function getSiteOrigin(): string {
-  let configuredOrigin: string;
-  if (process.env.WORKERS_CI_BRANCH && process.env.WORKERS_CI_BRANCH != "main") {
-    configuredOrigin = `https://${process.env.WORKERS_CI_BRANCH}-oblique-strategies-w.igormaka.workers.dev/`;
-  } else {
-    configuredOrigin = process.env.CF_PAGES_URL || process.env.SITEMAP_BASE_URL;
-  }
+  const configuredOrigin = process.env.BASE_URL;
 
   if (!configuredOrigin) {
-    throw new Error("WORKERS_CI_BRANCH or CF_PAGES_URL or SITEMAP_BASE_URL environment variable is required");
+    throw new Error("BASE_URL environment variable is required");
   }
 
   return configuredOrigin.replace(/\/$/, "");
